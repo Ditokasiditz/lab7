206dde28be0ce99736307ee0f7fc1127ac401563
Ditokasiditz <kasidit.samad@gmail.com>
2022-10-04 21:26:20 +0700

    complete

--- lab7.c
+++ lab7.c
@@ -42,21 +42,17 @@ void clear_bullet(int x,int y){
 }
 
 void draw_star(){
-	int xs,ys,i,m,n;
+	int xs,ys,i;
 	srand(time(NULL));
 	for(i=0;i<20;i++){
 		ys=rand(); ys=xs%10;
 		xs=rand(); xs=xs%100;
 	
 		if(ys>5 || ys<2){
-			while(ys>5 || ys<2){
-				ys=rand();ys=ys%10;
-			}
+			while(ys>5 || ys<2){ys=rand();ys=ys%10;	}
 		}
 		if(xs<10 || xs>70){
-			while(xs<10 || xs>70){
-				xs=rand(); xs=xs%100;
-			}
+			while(xs<10 || xs>70){xs=rand(); xs=xs%100;	}
 		}
 		gotoxy(xs,ys);
 		setcolor(7,0);
@@ -64,14 +60,46 @@ void draw_star(){
 	}
 }
 
+void fill_star(){
+	int xs,ys,i;
+	ys=rand(); ys=xs%10;
+	xs=rand(); xs=xs%100;
+	
+	if(ys>5 || ys<2){
+		while(ys>5 || ys<2){ys=rand();ys=ys%10;	}
+	}
+	if(xs<10 || xs>70){
+		while(xs<10 || xs>70){xs=rand(); xs=xs%100;	}
+	}
+	gotoxy(xs,ys);
+	setcolor(7,0);
+	printf("*");
+}
+
+char cursor(int x, int y){
+	HANDLE hStd = GetStdHandle(STD_OUTPUT_HANDLE);
+	char buf[2]; COORD c = {x,y}; DWORD num_read;
+	if(!ReadConsoleOutputCharacter(hStd,(LPTSTR)buf,1,c,(LPDWORD)&num_read) )
+		return '\0';
+	else
+		return buf[0];
+}
+
+void change_score(int x){
+	gotoxy(92,0);setcolor(7,0);printf("%d",x);
+}
 
 int main(){
 	char ch='.';
 	int x=38,y=25;
 	int bullet = 0,bx,by;
+	int score=0;
+	setcursor(0);
 	draw_star();
 	draw_ship(x,y);
-	setcursor(0);
+	gotoxy(85,0);setcolor(7,0);printf("score:");
+	change_score(score);
+	
 	do {
 		if (_kbhit()){
 			ch=_getch();
@@ -83,7 +111,20 @@ int main(){
 		
 	if (bullet==1) {
 		clear_bullet(bx,by);
-		if (by==2) { bullet=0;} else { draw_bullet(bx,--by);Beep(8000,100);  }
+		if (by==2) { bullet=0;} 
+		else { 
+			if(cursor(bx,by-1)=='*'){
+				Beep(9000,200);
+				clear_bullet(bx,by);
+				gotoxy(bx,by-1);printf(" ");
+				fill_star();
+				score=score+100;
+				change_score(score);
+				bullet=0;
+			}
+			else{
+				draw_bullet(bx,--by);
+				Beep(5000,100);}  }
 	}
 	Sleep(100);
 } while (ch!='x');
